41f17ece0e04bfbe01f3965626e69ece7dcb92cf
maninder-singh <nsit.maninder@gmail.com>
2015-08-26 15:44:49 +0530

    add DATA,MESSAGE,STATUS field

--- src/main/java/com/imaginea/user_registration/constant/Constant.java
+++ src/main/java/com/imaginea/user_registration/constant/Constant.java
@@ -6,4 +6,9 @@ package com.imaginea.user_registration.constant;
 public class Constant {
 
     public static final String FILE_NAME_TO_LOAD = "config.properties"; // separate properties file name with space
+    public static final String DATA = "data";
+    public static final String MESSAGE = "message";
+    public static final String STATUS = "status";
+    public static final String OK = "ok";
+    public static final String FAIL = "fail";
 }
