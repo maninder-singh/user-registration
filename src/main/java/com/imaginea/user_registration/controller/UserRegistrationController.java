025436ed5b74cd7c420780a3afc2800439757423
maninder-singh <nsit.maninder@gmail.com>
2015-08-26 15:46:25 +0530

    change return method to Map<String,Object>

--- src/main/java/com/imaginea/user_registration/controller/UserRegistrationController.java
+++ src/main/java/com/imaginea/user_registration/controller/UserRegistrationController.java
@@ -1,86 +1,133 @@
 package com.imaginea.user_registration.controller;
 
+import com.imaginea.user_registration.constant.Constant;
+import com.imaginea.user_registration.constant.UIMessageConstant;
 import com.imaginea.user_registration.pojo.User;
 import com.imaginea.user_registration.service.UserRegistrationService;
+import org.apache.log4j.Logger;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by maninders on 25/8/15.
  */
 
 @Path("/registration")
-@Produces(MediaType.APPLICATION_JSON)
 public class UserRegistrationController {
 
     private UserRegistrationService userRegistrationService = new UserRegistrationService();
+    static final Logger log = Logger.getLogger(UserRegistrationController.class);
 
     @GET
     @Path("/get-all-users")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<User> getAllUsers(){
+    public Map<String,Object> getAllUsers(){
         List<User> userList = new ArrayList<User>();
+//        userRegistrationService = new UserRegistrationService();
         try{
             userList = userRegistrationService.getAllUsers();
+            return getResultMap("",Constant.OK,userList);
         }catch (ClassNotFoundException e) {
-
+            log.error("ClassNotFoundException : " + e);
         } catch (SQLException e) {
-
+            log.error("SQLException : " + e);
         }
-        return userList;
+//        Map<String,Object> map = new HashMap<String, Object>();
+//        map.put("list",userList);
+//        return map;
+        return getResultMap(UIMessageConstant.GET_ALL_USER_MSG,Constant.FAIL,"");
     }
 
     @GET
     @Path("/get-user/{userId}")
-    public User getUser(@PathParam("userId") int userId){
-        User user = new User();
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String,Object> getUser(@PathParam("userId") int userId){
+        User user;
         try {
             user = userRegistrationService.getUser(userId);
+            return getResultMap("",Constant.OK,user);
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("SQLException : " + e);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            log.error("ClassNotFoundException : " + e);
         }
-        return user;
+        return getResultMap(UIMessageConstant.GET_ALL_USER_MSG,Constant.FAIL,"");
     }
 
     @POST
     @Path("/add-user")
-    public void insertUser(@PathParam("user") User user) {
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Map<String,Object> insertUser(@PathParam("user") User user) {
         try {
             userRegistrationService.insertUser(user);
+            return getResultMap("",Constant.OK,"");
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("SQLException : " + e);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            log.error("ClassNotFoundException : " + e);
         }
+        return getResultMap(UIMessageConstant.INSERT_USER_MSG + user.getFirstName() + user.getLastName()
+                ,Constant.FAIL,"");
     }
 
     @PUT
     @Path("/update-user")
-    public void updateUser(@PathParam("user") User user){
+    public Map<String,Object> updateUser(@PathParam("user") User user){
         try {
             userRegistrationService.updateUser(user);
+            return getResultMap("", Constant.OK,"");
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("SQLException : " + e);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            log.error("ClassNotFoundException : " + e);
         }
+        return getResultMap(UIMessageConstant.UPDATE_USER_MSG + user.getFirstName() + user.getLastName()
+                ,Constant.FAIL,"");
     }
 
     @DELETE
-    @Path("/delete-user")
-    public void deleteUser(@PathParam("user") User user){
+    @Path("/delete-user/{userId}")
+    public Map<String,Object> deleteUser(@PathParam("userId") int userId){
         try {
-            userRegistrationService.deleteUser(user);
+            userRegistrationService.deleteUser(userId);
+            return getResultMap("",Constant.OK,"");
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("SQLException : " + e);
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            log.error("ClassNotFoundException : " + e);
         }
+        return getResultMap(UIMessageConstant.DELETE_USER_MSG,Constant.FAIL,"");
+    }
+
+    public Map<String,Object> getResultMap(String message,String status,Object data){
+        Map<String,Object> resultMap = new HashMap<String, Object>();
+        resultMap.put(Constant.DATA,data);
+        resultMap.put(Constant.MESSAGE,message);
+        resultMap.put(Constant.STATUS,status);
+        return resultMap;
+    }
+    /**
+     * Test method to check service is working or not
+     */
+    @GET
+    @Path("/{param}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Map<String, Object> getMsg(@PathParam("param") String msg) {
+        String output = "Jersey say : " + msg;
+        Map<String ,Object> resultMap = new HashMap<String, Object>();
+        resultMap.put("message", output);
+        List<String> list = new ArrayList<String>();
+        list.add(output);
+        list.add(output + "append second time");
+        resultMap.put("list", list);
+        resultMap.put("service-data",userRegistrationService.getUserList());
+        return resultMap;
     }
 }
