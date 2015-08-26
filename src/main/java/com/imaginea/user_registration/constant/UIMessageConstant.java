bcd23620320e4a61b66f7d61861d9a172ba896c5
maninder-singh <nsit.maninder@gmail.com>
2015-08-26 15:42:58 +0530

    new file

---
+++ src/main/java/com/imaginea/user_registration/constant/UIMessageConstant.java
@@ -0,0 +1,11 @@
+package com.imaginea.user_registration.constant;
+
+/**
+ * Created by maninders on 26/8/15.
+ */
+public class UIMessageConstant {
+    public static final String GET_ALL_USER_MSG = "Unable to get the data";
+    public static final String INSERT_USER_MSG = "Unable to insert user : ";
+    public static final String DELETE_USER_MSG = "Unable to delete user : ";
+    public static final String UPDATE_USER_MSG = "Unable to update user : ";
+}
