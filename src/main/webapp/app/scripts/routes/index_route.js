2a109e7bdedf058d3a8b59dbb363710a71fdeda4
maninder-singh <nsit.maninder@gmail.com>
2015-08-31 17:17:49 +0530

    redirect from index to get-all-users route

--- src/main/webapp/app/scripts/routes/index_route.js
+++ src/main/webapp/app/scripts/routes/index_route.js
@@ -3,9 +3,8 @@
  */
 
 UserRegistrationApp.IndexRoute = Ember.Route.extend({
-//    model : function () {
-//        return ["red","blue","green"];
-//    }
-
+    afterModel : function(model, transition){
+        this.transitionTo('get-all-users');
+    }
 });
 
