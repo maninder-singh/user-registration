e8bfb6b88ae685c8de592168b34ad380ec23ab81
maninder-singh <nsit.maninder@gmail.com>
2015-08-31 12:22:12 +0530

    fix transitionToRoute bug

--- src/main/webapp/app/scripts/controllers/add-new-controller.js
+++ src/main/webapp/app/scripts/controllers/add-new-controller.js
@@ -21,6 +21,7 @@ UserRegistrationApp.AddUserController = Ember.Controller.extend({
     }.observes('firstName','middleName','lastName'),
     actions : {
         addNewUser : function(){
+            var self = this;
             var user = {};
             user["firstName"] = this.get('firstName');
             user["middleName"] = this.get('middleName');
@@ -39,22 +40,22 @@ UserRegistrationApp.AddUserController = Ember.Controller.extend({
                 contentType:"application/json; charset=utf-8",
                 success: function(data){
                     console.log("success");
+                    self.transitionToRoute('get-all-users');
+                    self.set('firstName',"");
+                    self.set('middleName',"");
+                    self.set('lastName',"");
+                    self.set('email',"");
+                    self.set('address',"");
+                    self.set('city',"");
+                    self.set('state',"");
+                    self.set('zip',"");
+                    self.set('country',"");
                 },
                 error: function (jqXHR, textStatus, errorThrown){
                     console.log("error");
                 },
                 dataType: "json"
             });
-            this.transitionToRoute('get-all-users');
-            this.set('firstName',"");
-            this.set('middleName',"");
-            this.set('lastName',"");
-            this.set('email',"");
-            this.set('address',"");
-            this.set('city',"");
-            this.set('state',"");
-            this.set('zip',"");
-            this.set('country',"");
         }
     }
 });
