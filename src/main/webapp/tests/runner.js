f4e81a37b3a5cee47a603f84f40003ebf5bbe126
maninder-singh <nsit.maninder@gmail.com>
2015-09-01 17:55:20 +0530

    add ember test cases

--- src/main/webapp/tests/runner.js
+++ src/main/webapp/tests/runner.js
@@ -5,9 +5,9 @@ if (window.location.search.indexOf("?tests") !== -1) {
     '<div id="ember-testing-container">' +
     '  <div id="ember-testing"></div>' +
     '</div>' +
-    '<link rel="stylesheet" href="runner.css">' +
-    '<link rel="stylesheet" href="vendor/qunit-1.12.0.css">' +
-    '<script src="vendor/qunit-1.12.0.js"></script>' +
-    '<script src="tests.js"></script>'
+    '<link rel="stylesheet" href="tests/runner.css">' +
+    '<link rel="stylesheet" href="tests/vendor/qunit-1.12.0.css">' +
+    '<script src="tests/vendor/qunit-1.12.0.js"></script>' +
+    '<script src="tests/tests.js"></script>'
   )
 }
--- src/main/webapp/tests/tests.js
+++ src/main/webapp/tests/tests.js
@@ -14,18 +14,56 @@ module("Integration tests", {
 
   teardown: function() {
     // reset the application state between each tests
-    UserRegistrationApp.reset();
+    UserRegistrationApp.reset();    
   }
 });
 
 // QUnit tests case
-test("/", function() {
+test("get-all-users template", function() {
   // async helper telling the application to go to the '/' route
-  visit("/");
+  visit("/get-all-users");
 
   // helper waiting the application is idle before running the callback
   andThen(function() {
-    equal(find("h2").text(), "Welcome to Ember.js", "Application header is rendered");
-    equal(find("li").length, 3, "There are three items in the list");
+    equal(currentRouteName(),"get-all-users","route name is get-all-users");
+    equal(currentURL(),"/get-all-users","url is /get-all-users");
+  });
+});
+
+
+
+test("add-user template", function() {
+  // async helper telling the application to go to the '/' route
+  
+  visit("/get-all-users");
+  var listOfUser;
+  andThen(function(){
+      listOfUser = find("table tr").length;
+  });
+  
+
+  visit("/add-user");
+
+  // helper waiting the application is idle before running the callback
+  andThen(function() {
+    equal(currentRouteName(),"add-user","route name is add-user");
+    equal(currentURL(),"/add-user","url is /add-user");
+    
+    fillIn('#firstName', "firstName");
+    fillIn('#middleName', "middleName");
+    fillIn('#lastName', "lastName");
+    fillIn('#email', "email@email.com");
+    fillIn('#address', "address");
+    fillIn('#city', "city");
+    fillIn('#state', "state");
+    fillIn('#zip', "500034");  
+    fillIn('#country', "country");
+    click('button.btn-success');
+  });
+
+  visit("/get-all-users");
+
+  andThen(function(){
+      equal(find("table tr").length,listOfUser + 1,"New user added successfully");
   });
 });
